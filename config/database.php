f50405068c073878f05d64bcadbda2575a7bb462
ghaff <mudashiruagm@gmail.com>
2019-07-10 06:55:37 -0400

    db conn

--- config/database.php
+++ config/database.php
@@ -52,7 +52,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
+            'url' => $url,
             'host' => $host,
             'port' => 3306,
             'database' => $database,
